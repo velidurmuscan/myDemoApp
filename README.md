6864c80bd98cd90ca9cdea6fbad920bcc6a0a60a
Veli Durmuscan <durmuscanveli@gmail.com>
2018-09-29 21:19:05 +0300

    First commit for this demo project

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the README.md file of this demo project.
+This content is unimportant.
+Author: Veli Durmuşcan
---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,28 @@
+package com.mycompany.app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+	public static boolean search(ArrayList<Integer> array, int e) {
+        System.out.println("inside search");
+        if (array == null)
+            return false;
+
+        for (int elt : array) {
+            if (elt == e)
+                return true;
+        }
+        return false;
+    }
+}
